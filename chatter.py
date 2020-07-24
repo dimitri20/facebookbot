66e384b897571899a7ba7aeb5d64e5a8307112d2
dimitri20 <dimitri.gulua@geolab.edu.ge>
2020-07-24 21:55:05 +0400

    add

--- chatter.py
+++ chatter.py
@@ -1,24 +1,58 @@
-from fbchat import *
+import tkinter as tk
+from tkinter import *
 from fbchat.models import Message
-import PySimpleGUI as sg
+from fbchat import *
+
+username = "dito.gulua@mail.ru"
+password = "ZolokosTi!04"
+
+client = Client(username, password)
+
+root = tk.Tk()
+root.configure(bg="white")
+root.geometry("650x600")
+root.resizable(False, False)
 
 
-sg.theme('DarkAmber')
+def onClickPlaceholder(event):
+    userCommand.configure(state=NORMAL)
+    userCommand.delete(0, END)
+    userCommand.unbind('<Button-1>', on_click_id)
 
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] 
+def onClickSearch():
+	friends = []
+	print("Searching...")
+	
+	for human in client.searchForUsers(user):
+		if human.is_friend:
+			friends.append(human)
+	print(f"found {len(friends)} friend")	
+	for friend in friends:
+		print(f"{friend.name}  --  {friend.uid} \n")
 
-        	]
+	label = tk.Label(root, text=f"{friend.name}  --  {friend.uid} \n")
+	label.pack()
 
-window = sg.Window('Window Title', layout)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
 
-window.close()
+
+userCommandBorder = tk.Frame(root, background = 'orange')
+userCommandPadding = tk.Frame(userCommandBorder, background = 'white')
+
+userCommand = tk.Entry(userCommandPadding, width=55, font='Arial 12', bd=0)
+userCommand.insert(0, "Enter Friend name")
+userCommand.configure(state=DISABLED)
+on_click_id = userCommand.bind('<Button-1>', onClickPlaceholder)
+
+userCommandPadding.pack(padx=4, pady=4)
+userCommandBorder.pack(pady=60)
+userCommand.pack(fill="both", expand=True, padx=12, pady=7, side=LEFT)
+
+photo = PhotoImage(file = "next (2).png")
+goToResult = tk.Button(userCommandPadding, text="Search", image=photo, width=25, height=25, command=onClickSearch, bd=0, bg='white')
+goToResult.pack(side=RIGHT, padx=5)
+
+
+root.mainloop()
 
 
 """
