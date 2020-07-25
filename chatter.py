f6da8db21a6e5f0073f367714a4cd7b55291894e
dimitri20 <dimitri.gulua@geolab.edu.ge>
2020-07-25 10:24:55 +0400

    ahh

--- chatter.py
+++ chatter.py
@@ -1,84 +1,64 @@
-import tkinter as tk
-from tkinter import *
 from fbchat.models import Message
-from fbchat import *
-
-username = "dito.gulua@mail.ru"
-password = "ZolokosTi!04"
-
-client = Client(username, password)
-
-root = tk.Tk()
-root.configure(bg="white")
-root.geometry("650x600")
-root.resizable(False, False)
+from fbchat import log, Client
 
 
-def onClickPlaceholder(event):
-    userCommand.configure(state=NORMAL)
-    userCommand.delete(0, END)
-    userCommand.unbind('<Button-1>', on_click_id)
+# username = "dito.gulua@mail.ru"
+# password = "ZolokosTi!04"
 
-def onClickSearch():
-	friends = []
-	print("Searching...")
+# client = Client(username, password)
+
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
+
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If you're not the author, echo
+        if author_id != self.uid:
+            self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+
+
+
+
+class Facebook():
+	def __init__(self, username, password):
+		self.client = Client(username, password)
+
+	def autoReply(self):
+		income = self.client.listen()
+		author = income.author
+		if onMessage():
+			self.client.send(Message(text=income.text), thread_id=author)
+
+
+	def findFriend(self, name):
+		friends = []
+		for human in self.client.searchForUsers(name):
+			if human.is_friend:
+				friends.append({
+					"username" : human.name,
+					"uid" : human.uid,
+					"photo" : human.photo
+					})
+		
+		return friends
+
+	def sendMessage(self):
+		self.client.send(Message(text="Hi me!"), thread_id=client.uid, thread_type=ThreadType.USER)
+
+
+	def logout(self):
+		self.client.logout()
+
+
+def main():
+	a = Facebook("dito.gulua@mail.ru", "ZolokosTi!04")
+	a.autoReply()
 	
-	for human in client.searchForUsers(user):
-		if human.is_friend:
-			friends.append(human)
-	print(f"found {len(friends)} friend")	
-	for friend in friends:
-		print(f"{friend.name}  --  {friend.uid} \n")
 
-	label = tk.Label(root, text=f"{friend.name}  --  {friend.uid} \n")
-	label.pack()
+if __name__ == '__main__':
+	client = EchoBot("dito.gulua@mail.ru", "ZolokosTi!04")
+	client.listen()
 
 
-
-userCommandBorder = tk.Frame(root, background = 'orange')
-userCommandPadding = tk.Frame(userCommandBorder, background = 'white')
-
-userCommand = tk.Entry(userCommandPadding, width=55, font='Arial 12', bd=0)
-userCommand.insert(0, "Enter Friend name")
-userCommand.configure(state=DISABLED)
-on_click_id = userCommand.bind('<Button-1>', onClickPlaceholder)
-
-userCommandPadding.pack(padx=4, pady=4)
-userCommandBorder.pack(pady=60)
-userCommand.pack(fill="both", expand=True, padx=12, pady=7, side=LEFT)
-
-photo = PhotoImage(file = "next (2).png")
-goToResult = tk.Button(userCommandPadding, text="Search", image=photo, width=25, height=25, command=onClickSearch, bd=0, bg='white')
-goToResult.pack(side=RIGHT, padx=5)
-
-
-root.mainloop()
-
-
-"""
-username = "dito.gulua@mail.ru"
-password = "ZolokosTi!04"
-
-client = Client(username, password)
-
-
-while True:
-	friends = []
-	user = input("Enter username:  ")
-	if user == 'exit':
-		break
-	print("Searching...")
-	
-	for human in client.searchForUsers(user):
-		if human.is_friend:
-			friends.append(human)
-	print(f"found {len(friends)} friend")	
-	for friend in friends:
-		print(f"{friend.name}  --  {friend.uid} \n")
-
-	# print(client.searchForUsers(user)[0].name, 'uid = ', client.searchForUsers(user)[0].uid)
-
-client.logout()
-
-
-"""
